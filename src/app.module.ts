01872a114fbf5c158c8c4ede6c2cb85377a415aa
Mootaz12 <mootazbellehweslati@gmail.com>
2026-02-22 21:52:26 +0100

    add global bullMQ configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,34 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { BullModule } from '@nestjs/bullmq';
 import { UrlShortenerModule } from '@modules/url-shortener/url-shortener.module';
 import { UsersModule } from '@modules/users/users.module';
 import { dataSourceOptions } from './database/data-source';
+import { RedisConfig, redisConnectionConfig } from './config/redis.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      load: [redisConnectionConfig],
     }),
     TypeOrmModule.forRoot({
       ...dataSourceOptions,
       autoLoadEntities: true,
+    }),
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        const redisConfig = configService.getOrThrow<RedisConfig>('redis');
+        return {
+          connection: {
+            host: redisConfig.host,
+            port: redisConfig.port,
+          },
+        };
+      },
+      inject: [ConfigService],
     }),
     UrlShortenerModule,
     UsersModule,
